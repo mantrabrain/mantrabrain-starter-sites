f214c011a84f33e4e53a5005009a215747c55ad8
Umesh Ghimire <me.umeshuser@gmail.com>
2022-01-10 23:00:14 +0545

    Update mantrabrain-starter-sites.php
    Version update

--- mantrabrain-starter-sites.php
+++ mantrabrain-starter-sites.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Mantrabrain Starter Sites
  * Description: Starter sites / Demo importer for Mantra Brain Themes
- * Version: 1.1.2
+ * Version: 1.1.3
  * Author: Mantrabrain
  * Author URI: https://mantrabrain.com
  * License: GPLv3 or later
