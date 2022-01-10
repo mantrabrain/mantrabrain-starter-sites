050cf5d1a1b59d026c3f690e6ccdf740c951e667
Umesh Ghimire <me.umeshuser@gmail.com>
2022-01-10 23:02:17 +0545

    Update class-mantrabrain-starter-sites.php
    Issue fux

--- includes/class-mantrabrain-starter-sites.php
+++ includes/class-mantrabrain-starter-sites.php
@@ -21,7 +21,7 @@ final class Mantrabrain_Starter_Sites
      *
      * @var string
      */
-    public $version = '1.1.2';
+    public $version = '1.1.3';
 
     /**
      * Theme single instance of this class.
@@ -150,7 +150,7 @@ final class Mantrabrain_Starter_Sites
 
         if (is_admin()) {
             include_once MANTRABRAIN_STARTER_SITES_ABSPATH . 'includes/class-mantrabrain-demo-importer.php';
-            include_once MANTRABRAIN_STARTER_SITES_ABSPATH . 'includes/admin/class-mantrabrain-admin-notices.php';
+            //include_once MANTRABRAIN_STARTER_SITES_ABSPATH . 'includes/admin/class-mantrabrain-admin-notices.php';
             include_once MANTRABRAIN_STARTER_SITES_ABSPATH . 'includes/functions.php';
             //Dashboard
             include_once MANTRABRAIN_STARTER_SITES_ABSPATH . 'includes/admin/dashboard/class-mantrabrain-admin-dashboard.php';
